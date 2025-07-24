941ac26ea0358311f00c816c362a951199119450
mugund10 <bjmugundhan@gmail.com>
2025-07-24 15:31:45 +0530

    keys update

--- keys.go
+++ keys.go
@@ -35,7 +35,6 @@ func (k *keys) Save(filename string) error {
 
 // marshals privates key
 func (k *keys) prConvAndSave(filename string) bool {
-
 	privbyte, err := x509.MarshalPKCS8PrivateKey(k.private)
 	if err != nil {
 		log.Println(err)
@@ -52,12 +51,10 @@ func (k *keys) prConvAndSave(filename string) bool {
 		return false
 	}
 	return true
-
 }
 
 // marshals pubic key
 func (k *keys) puConvAndSave(filename string) bool {
-
 	publicbyte, err := x509.MarshalPKIXPublicKey(&k.private.PublicKey)
 	if err != nil {
 		log.Println(err)
@@ -74,7 +71,6 @@ func (k *keys) puConvAndSave(filename string) bool {
 		return false
 	}
 	return true
-
 }
 
 // saves file with the given name
@@ -88,18 +84,6 @@ func saveAsFile(name string, byt []byte) error {
 
 // Loads keys from pem files
 func Loadkeys(filename string) (keys, error) {
-	// pubPEM, err := readfile(fmt.Sprintf("%s_publickey.pem", filename))
-	// if err != nil {
-	// 	return keys{}, err
-	// }
-	// pbkey, err := x509.ParsePKIXPublicKey(pubPEM.Bytes)
-	// if err != nil {
-	// 	return keys{}, err
-	// }
-	// pub, ok := pbkey.(*ecdsa.PublicKey)
-	// if !ok {
-	// 	return keys{}, fmt.Errorf("not an ECDSA public key")
-	// }
 	privPEM, err := readfile(fmt.Sprintf("%s_privatekey.pem", filename))
 	if err != nil {
 		return keys{}, err
@@ -110,9 +94,8 @@ func Loadkeys(filename string) (keys, error) {
 	}
 	priv, ok := prkey.(*ecdsa.PrivateKey)
 	if !ok {
-		return keys{}, fmt.Errorf("not an ECDSA private key")
+		return keys{}, fmt.Errorf("[error] Its not an ecdsa privatekey")
 	}
-
 	return keys{priv}, nil
 }
 
@@ -127,4 +110,8 @@ func readfile(filename string) (*pem.Block, error) {
 		return nil, fmt.Errorf("[error] failed to decode PEM block from file : %s", filename)
 	}
 	return pblock, nil
+}
+
+func (k *keys) GetKeys() *ecdsa.PrivateKey {
+	return k.private
 }
